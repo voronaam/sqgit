61482d3de0fa76c3449b051b9d22d434d61adeb0
Aleksey Vorona <avorona@xmatters.com>
2015-09-04 14:08:17 -0700

    Ready to start parsing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 use std::process::Command;
 
 fn main() {
-    let params = vec!("--count", "HEAD");
+    let args: Vec<_> = std::env::args().collect();
+    if args.len() < 2 {
+        panic!("Please provide a query")
+    }
+    let query = &args[1];
+    println!("Executing query: {}", query);
+    
+    let params = vec!("HEAD");
     let (status, output) = rev_list(&params);
-    println!("process exited with: {}", status);
-    println!("{}", output);
+    if !status.success() {
+        println!("process exited with: {}", status);
+    } else {
+        format_output(&output);
+    }
 }
 
 fn rev_list(params: &Vec<&str>) -> (std::process::ExitStatus, String) {
@@ -13,8 +23,9 @@ fn rev_list(params: &Vec<&str>) -> (std::process::ExitStatus, String) {
     let output = git.output().unwrap_or_else(|e| {
         panic!("failed to execute git: {}", e)
     });
-    let stdout = String::from_utf8_lossy(&output.stdout).to_string();
-    //(output.status, String::from_utf8_lossy(&output.stdout))
-    (output.status, stdout)
+    (output.status, String::from_utf8_lossy(&output.stdout).to_string())
 }
     
+fn format_output(output: &String) {
+    println!("{}", output);
+}
