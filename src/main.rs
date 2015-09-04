6688e3c8023a85f5aebfef61f292eb5aa5cd4f59
Aleksey Vorona <avorona@xmatters.com>
2015-09-04 13:39:53 -0700

    Works!

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 use std::process::Command;
 
 fn main() {
-    let status = rev_list("--count");
+    let params = vec!("--count", "HEAD");
+    let (status, output) = rev_list(&params);
     println!("process exited with: {}", status);
+    println!("{}", output);
 }
 
-fn rev_list(params: &str) -> std::process::ExitStatus {
-    let command = format!("git ref-list {}", params);   
-    println!("{}", command);
-    let mut git = Command::new(command);
-    let status = git.status().unwrap_or_else(|e| {
+fn rev_list(params: &Vec<&str>) -> (std::process::ExitStatus, String) {
+    let mut git = Command::new("git");
+    git.arg("rev-list").args(params);
+    let output = git.output().unwrap_or_else(|e| {
         panic!("failed to execute git: {}", e)
     });
-    status
+    let stdout = String::from_utf8_lossy(&output.stdout).to_string();
+    //(output.status, String::from_utf8_lossy(&output.stdout))
+    (output.status, stdout)
 }
     
